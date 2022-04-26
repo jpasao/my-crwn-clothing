b5cea2d09aa3b48bc68e0293e744f8ab023b1b6f
Juan Pablo Sánchez Olivares <juan.pablo.sanchez.olivares@everis.com>
2022-04-26 09:03:26 +0200

    added stripe payment method

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
 import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 import './index.scss';
 
@@ -14,8 +16,10 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>        
-          <App />        
+        <BrowserRouter>  
+          <Elements stripe={stripePromise}>
+            <App />          
+          </Elements>      
         </BrowserRouter>
       </PersistGate>
     </Provider>
